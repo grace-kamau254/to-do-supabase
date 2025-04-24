7b31378f249199f9f84c54105367c7e4cb8f6912
grace-kamau254 <kamaugraciie@gmail.com>
2025-04-24 20:06:49 +0300

    added a supabase file

--- Task Manager/src/supabase-clients.ts
+++ Task Manager/src/supabase-clients.ts
@@ -1,3 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient()
+
+const SUPABASE_URL ='https://fkphmprxuzuxhvelhnhc.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrcGhtcHJ4dXp1eGh2ZWxobmhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMTM4MTgsImV4cCI6MjA2MDg4OTgxOH0.Zq3GMcPBoEeSEeWQw9GT0cjj0V4ILSo9VSElb8WtZSU'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
